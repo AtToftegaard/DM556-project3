55e6672facf67bc3f52cdb78ccc0ad8bad5848eb
ICantC <Andreas@toftegaard.org>
2017-05-02 13:43:09 +0200

    Select done

--- query/Select.java
+++ query/Select.java
@@ -1,26 +1,40 @@
 package query;
 
+import heap.HeapFile;
 import parser.AST_Select;
+import relop.FileScan;
+import relop.Iterator;
 import relop.Predicate;
+import relop.Projection;
 import relop.Schema;
+import relop.Selection;
+import relop.SimpleJoin;
 
 /**
  * Execution plan for selecting tuples.
  */
 class Select implements Plan {
 
-	 /** Name of tables to select from. */
+	 // Name of tables to select from. 
 	  protected String[] tableNames;
 
 	 // Column names to select from.
 	  protected String[] columns;
 	  
-	 // Schema of the table to insert into. 
+	 // Schemas of the tables to insert into. 
+	  protected Schema[] schemas;
+	  
+	 // Current schema
 	  protected Schema schema;
 
-	  // Actual values to insert into the table. 
-	  protected Predicate[][] predicates[]; 
-	
+	 // Predicates to select by
+	  protected Predicate[][] predicates; 
+	  
+	 // Boolean for if we want execute or explain
+	  public boolean bool;
+	  
+	 // The iterator
+	  protected Iterator iter;
 	
   /**
    * Optimizes the plan, given the parsed query.
@@ -29,28 +43,70 @@ class Select implements Plan {
    */
   public Select(AST_Select tree) throws QueryException {
 	  
-	  // Validate table, columns and predicates
-	    tableNames = tree.getTables();
-	    for (String table : tableNames){
-	    	schema = QueryCheck.tableExists(table);
-	    	
-	    	for (String columnName : columns){
-	    		QueryCheck.columnExists(schema, columnName);	
-	    	}
-	    	for (Predicate[][] predicate : predicates){
-	    		QueryCheck.predicates(schema, predicate);
-	    	}
-	    }
-	  
+	    this.bool = tree.isExplain;
+	    this.select(tree);
+	    this.select();
+	       
   } // public Select(AST_Select tree) throws QueryException
 
-  /**
+  private void select() {
+	  
+	this.iter = new FileScan(this.schemas[0], new HeapFile(this.tableNames[0]));
+	int n = 1;
+	while (n < this.tableNames.length){
+		this.iter = new SimpleJoin(this.iter, new FileScan(this.schemas[n], new HeapFile(this.tableNames[n])), new Predicate[0]);
+		++n;
+	}
+	n = 0;
+	while (n < this.predicates.length){
+		this.iter = new Selection(iter, this.predicates[n]);
+		++n;
+	}
+	if (this.columns.length > 0){
+		Integer[] intarr = new Integer[this.columns.length];
+		int index = 0;
+		while (index < this.columns.length){
+			intarr[index] = this.schema.fieldNumber(this.columns[index]);
+			++index;
+		}
+		this.iter = new Projection(this.iter, intarr);
+	}
+  }
+
+  private void select(AST_Select tree) throws QueryException {
+	  
+	this.schema = new Schema(0);
+	this.tableNames = tree.getTables(); 
+	this.schemas = new Schema[this.tableNames.length];
+	int n = 0;
+	
+	while (n < this.tableNames.length){
+		this.schemas[n] = QueryCheck.tableExists(tableNames[n]);
+		this.schema = Schema.join(schema, this.schemas[n]);
+		++n;
+	}
+	columns = tree.getColumns();
+	int index = 0;
+	int end = columns.length;
+	while (index < end){
+		QueryCheck.columnExists(schema, columns[index]);
+		++index;
+	}
+	this.predicates = tree.getPredicates();
+	QueryCheck.predicates(schema, this.predicates);
+  }
+
+/**
    * Executes the plan and prints applicable output.
    */
   public void execute() {
 
-    // print the output message
-    System.out.println("0 rows affected. (Not implemented)");
+	  if (this.bool){
+		  this.iter.explain(0);
+	  } else {
+		  int n = this.iter.execute();
+		  System.out.print("\n" + n + " rows affected.");
+	  }
 
   } // public void execute()
 
